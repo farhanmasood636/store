468694bb8cd5f608d31b9eeadb1f094089120d5d
Muhammad Umar <umar.sheikh.muhammad@gmail.com>
2024-02-13 00:28:34 +0500

    Update meta description

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,14 +19,7 @@ export default function RootLayout({
       <head>
         <meta
           title="description"
-          content="Direct promotion compares deals on Refurbished Cell Phones, iPhones, Tablets, iPads, Smartwatches, 
-          MacBooks & more. Direct promotion compares prices from the leading brands to help you find the best deals on 
-          Refurbished devices. Save yourself the time and hassle of shopping around by using directpromotion.online to compare all 
-          the best deals in one place. Direct promotion carries some of the best deals for refurbished phones in the USA, but one of 
-          the many winning factors for buying refurbished is the significantly reduced prices on refurbished models that work as great as
-           new! All our refurbished phones are professionally checked and tested with detailed quality checkpoints. We believe in transparency so
-            we make it a priority to always inform our customers of any cosmetic flaws that may be present with our refurbished phones. No matter 
-            the physical condition, all our devices are 100% functional and come with 15 days free check warranty."
+          content="At Direct Promotion, find the best deals on Refurbished Cell Phones, iPhones, Tablets, iPads, Smartwatches, MacBooks & more. Compare leading brands' prices and save time. All refurbished devices are thoroughly tested, ensuring functionality. Enjoy reduced prices and transparent information on cosmetic flaws. Shop now!"
         ></meta>
         <script
           async
